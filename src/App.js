3d1d97df1b5e584d9e3279458d4dccdeb50ff73b
saiyadm99 <syedmohammod52@gmail.com>
2022-01-03 22:24:24 +0600

    deploy

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
 	}
 
 	componentDidMount(){
-		fetch('http://localhost:3000')
+		fetch('https://secret-lake-59536.herokuapp.com/')
 		.then(response => response.json())
 		.then(console.log)
 	}
@@ -77,7 +77,7 @@ class App extends Component {
 
 	onButtonSubmit = () => {
 		this.setState({imageUrl: this.state.input})
-		fetch('http://localhost:3000/imageurl',{
+		fetch('https://secret-lake-59536.herokuapp.com/imageurl',{
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component {
 		.then(response => response.json())
 		.then(response => {
 			if(response){
-				fetch('http://localhost:3000/image',{
+				fetch('https://secret-lake-59536.herokuapp.com/image',{
 					method: 'put',
 					headers: {'Content-Type': 'application/json'},
 					body: JSON.stringify({
